60a0d7ef3aba1aa6d8dcf65820eb5e31dcb6cd34
chun-eric <eric.chun88800888@gmail.com>
2024-05-23 17:54:38 +0900

    fixed alot of card structural issues except grid and transitions

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import data from "./data";
-import "./portfolio.css";
+// import "./portfolio.css";
+import "./portfolio2.css";
 import Projects from "./Projects";
 import ProjectsCategories from "./ProjectsCategories";
 import { useState } from "react";
--- src/sections/portfolio/PortfolioCard.jsx
+++ src/sections/portfolio/PortfolioCard.jsx
@@ -1,24 +1,69 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const PortfolioCard = ({ project }) => {
   return (
-    <div className='portfolio__project overlay'>
-      <div className='portfolio__project-image '>
-        <img src={project.image} alt='Portfolio Project Image' />
-      </div>
-      <p className='project__title'>{project.title}</p>
-      <p className='card-description'>{project.desc}</p>
-      <div className='portfolio__project-cta'>
-        <a href={project.demo} className='btn sm' rel='noopener noreferrer'>
-          Demo
-        </a>
-        <a
-          href={project.github}
-          className='btn sm primary'
-          rel='noopener noreferrer'
-        >
-          GitHub
-        </a>
+    // <div className='portfolio__project '>
+    //   <div className='portfolio__project-image '>
+    //     <img
+    //       className='cover-image'
+    //       src={project.image}
+    //       alt='Portfolio Project Image'
+    //     />
+    //   </div>
+    //   <div className='overlay'></div>
+    //   <div className='project__description' onMouseEnter={{}}>
+    //     <div className='project__lowerthird'>
+    //       <p className='project__title'>{project.title}</p>
+    //       <p className='card-description'>{project.desc}</p>
+    //       <div className='portfolio__project-cta'>
+    //         <button className='btn skill-btn button-card'>
+    //           <p>
+    //             <a href={project.demo} className='' rel='noopener noreferrer'>
+    //               Demo
+    //             </a>
+    //           </p>
+    //         </button>
+    //         <button className='btn skill-btn button-card'>
+    //           <a href={project.github} rel='noopener noreferrer'>
+    //             GitHub
+    //           </a>
+    //         </button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className='project-cards'>
+      <div className='card'>
+        <div className='image-wrapper '>
+          <img
+            className='cover-image'
+            src={project.image}
+            alt='Portfolio Project Image'
+          />
+        </div>
+        <div className='overlay'></div>
+        <div className='project__description'>
+          <div className='project__lowerthird'>
+            <p className='project__title'>{project.title}</p>
+            <p className='card-description'>{project.desc}</p>
+            <div className='portfolio__project-cta'>
+              <button className='btn skill-btn button-card'>
+                <p>
+                  <a href={project.demo} className='' rel='noopener noreferrer'>
+                    Demo
+                  </a>
+                </p>
+              </button>
+              <button className='btn skill-btn button-card'>
+                <a href={project.github} rel='noopener noreferrer'>
+                  Details
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
