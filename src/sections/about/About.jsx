f95ded8f0d750da043d4d9aa9229ed42fc1d0647
chun-eric <eric.chun88800888@gmail.com>
2024-06-13 16:35:40 +0900

    updated the data for my projects

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -18,18 +18,17 @@ const About = ({ theme }) => {
         </div>
         <div className='bio-container'>
           <p className='bio-info'>
-            Currently based in Osaka, Japan for over a decade, but raised in
-            Sydney, Australia for even longer yet somehow born in Dubai, U.A.E,
-            my development, design and philosophy has been greatly influenced by
-            both Western and Eastrn cultures. I am especially influenced by both
-            modern and traditional Japanese fashion, culture, art and design.
-            When I am developing any digital products whether it be emails,
-            landing pages, websites or web apps, I strive to make it simple,
-            easy to understand yet practical. UX/UI is also very important to me
-            so I'm constantly improving my skills and knowledge to get the best
-            results from every project. My attention to detail and ability to
-            meet tight deadlines ensure that I deliver projects on time, within
-            budget, and to exact specifications.
+            Currently based in Japan, but raised in Australia, yet somehow born
+            in Dubai, U.A.E., my development, design, and philosophy have been
+            greatly influenced by both Western and Eastern cultures. I am
+            especially inspired by both modern and traditional Japanese fashion,
+            culture, art, and design. When developing digital products, whether
+            they be emails, landing pages, websites, or web apps, I strive to
+            make them simple, easy to understand, and practical. UX/UI is very
+            important to me, so I'm constantly improving my skills and knowledge
+            to achieve the best results in every project. My attention to detail
+            and ability to meet tight deadlines ensure that I deliver projects
+            on time, within budget, and to exact specifications.
           </p>
 
           <div className='button-container'>
