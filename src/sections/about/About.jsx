eb94d8dc7928f9264418f1c3b5c872c4f28db196
chun-eric <eric.chun88800888@gmail.com>
2024-06-22 08:41:01 +0900

    updated about bio info paragraph layout and content

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -19,17 +19,26 @@ const About = ({ theme }) => {
           </div>
           <div className='bio-container'>
             <p className='bio-info'>
-              An Australian currently based in Osaka, Japan, but born in Dubai,
-              my development, design, and philosophy have been greatly
-              influenced by Western and Eastern cultures. I am fascinated by the
-              striking contrast between modern and traditional Japanese culture,
-              fashion, art, and design. When developing digital products,
-              whether they be emails, landing pages, websites, or web apps, I
-              strive to make them simple, easy to understand, and practical.
-              UX/UI is also very important to me, so I'm constantly improving my
-              skills and knowledge to achieve the best results for my clients.
-              Feel free to get in touch with me.
+              Hi, I'm Eric Chun, an Australian currently based in Osaka, Japan,
+              but born in Dubai. My development, design, and overall philosophy
+              have been greatly influenced by Western and Eastern cultures. I am
+              particulary fascinated by the striking contrast between modern and
+              traditional Japanese culture, fashion, art, and design.
             </p>
+            <p className='bio-info'>
+              When developing digital products, whether it be emails, landing
+              pages, websites, or web apps, I strive to make them simple, easy
+              to understand, and practical. UX/UI is also very important to me,
+              so I'm constantly improving my skills and knowledge to achieve the
+              best results for my clients.
+            </p>
+            <p className='bio-info'>
+              I also have interests in cybersecurity especially in the area of
+              governance and risk management, developing weird micro SaaS
+              applications in my spare time and going to the beach as much as
+              possible. Feel free to get in touch with me.
+            </p>
+            <p className='bio-info'></p>
 
             <div className='button-container'>
               <a href='#contact'>
