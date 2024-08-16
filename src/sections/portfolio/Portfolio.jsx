e618362fc10cdfd9d82fea5e03322bb8cf9edef6
chun-eric <eric.chun88800888@gmail.com>
2024-08-16 14:47:13 +0900

    completed fade in of portfolio

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,13 +1,14 @@
-import { useState, forwardRef } from "react";
+import { useEffect, useState, forwardRef } from "react";
 import data from "./data";
 import "./portfolio.css";
 import Projects from "./Projects";
 import ProjectsCategories from "./ProjectsCategories";
 import PropTypes from "prop-types";
+import A0S from "aos";
+import "aos/dist/aos.css";
 
 const Portfolio = forwardRef((props, ref) => {
   const [projects, setProjects] = useState(data);
-
   const categories = ["All", ...new Set(data.map((item) => item.category))];
 
   const filterProjectsHandler = (category) => {
@@ -18,20 +19,41 @@ const Portfolio = forwardRef((props, ref) => {
     }
   };
 
+  useEffect(() => {
+    A0S.init({
+      duration: 1000,
+      offset: 200,
+    });
+  }, []);
+
   const { theme } = props;
   return (
     <section id='portfolio' className={theme} ref={ref}>
       <div className={`portfolio-overlay ${theme}`}></div>
       <div className='portfolio-content'>
-        <div className='skills-title'>
+        <div
+          className='skills-title'
+          data-aos='fade-in'
+          data-aos-delay='150'
+          data-aos-duration='1500'
+          data-aos-easing='ease-in'
+        >
           <h3 className={`project-title ${theme}`}>Projects</h3>
         </div>
         <ProjectsCategories
           categories={categories}
           onFilterProjects={filterProjectsHandler}
           theme={theme}
+         
         />
-        <div className={`container portfolio__container ${theme}`}>
+        <div
+          className={`container portfolio__container ${theme}`}
+          data-aos='fade-in'
+          data-aos-delay='350'
+          data-aos-offset='200'
+          data-aos-duration='1500'
+          data-aos-easing='ease-in-out'
+        >
           <Projects projects={projects} theme={theme} />
         </div>
       </div>
--- src/sections/portfolio/ProjectsCategories.jsx
+++ src/sections/portfolio/ProjectsCategories.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/prop-types */
 import CategoryButton from "./CategoryButton";
 import { useState } from "react";
+import A0S from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const ProjectsCategories = ({ categories, onFilterProjects, theme }) => {
   const [activeCategory, setActiveCategory] = useState("All");
@@ -10,9 +13,23 @@ const ProjectsCategories = ({ categories, onFilterProjects, theme }) => {
     onFilterProjects(category);
   };
 
+  useEffect(() => {
+    A0S.init({
+      duration: 1000,
+      offset: 200,
+    });
+  }, []);
+
   return (
     <section className='section-categorybutton'>
-      <div className={`portfolio__categories ${theme}`}>
+      <div
+        className={`portfolio__categories ${theme}`}
+        data-aos='fade-in'
+        data-aos-delay='350'
+        data-aos-offset='200'
+        data-aos-duration='1500'
+        data-aos-easing='ease-in-out'
+      >
         {categories.map((category) => (
           <CategoryButton
             key={category}
@@ -29,5 +46,3 @@ const ProjectsCategories = ({ categories, onFilterProjects, theme }) => {
 };
 
 export default ProjectsCategories;
-
-
