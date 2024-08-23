f4e188bc5b40691cac412903b211548940a0bfc5
chun-eric <eric.chun88800888@gmail.com>
2024-08-23 12:30:26 +0900

    resized skills images

--- src/sections/skills/data.js
+++ src/sections/skills/data.js
@@ -1,151 +1,89 @@
+import html from "../../assets/logos/png/html-5-min.png";
+import css from "../../assets/logos/png/css-min.png";
+import javascript from "../../assets/logos/png/js-min.png";
+import photoshop1 from "../../assets/logos/png/photoshop-min.png";
+import figma from "../../assets/logos/png/figma-min.png";
+import emailOnAcid from "../../assets/logos/png/email-on-acid-min.png";
+import wordpress from "../../assets/logos/png/wordpress-min.png";
+import git from "../../assets/logos/png/git-min.png";
+import tailwind from "../../assets/logos/png/tailwind-min.png";
+import linux from "../../assets/logos/png/linux-min.png";
+import react from "../../assets/logos/png/react-icon-min.png";
+import wireshark from "../../assets/logos/png/wireshark-icon-min.png";
+
 const data = [
   {
     id: 1,
     title: "HTML",
-    src: "https://i.ibb.co/3dGpJGz/html-1.png",
+    src: html,
     className: "skillsIcon",
   },
   {
     id: 2,
     title: "CSS",
-    src: "https://i.ibb.co/L6yRyZd/css-3.png",
+    src: css,
     className: "skillsIcon",
   },
   {
     id: 3,
     title: "JavaScript",
-    src: "https://i.ibb.co/1ZJXF96/logo-javascript.png",
+    src: javascript,
     className: "skillsIcon",
   },
   {
     id: 4,
     title: "Photoshop",
-    src: "https://i.ibb.co/p3Qyf2m/photoshop-cc-4.png",
+    src: photoshop1,
     className: "skillsIcon",
   },
   {
     id: 5,
     title: "Figma",
-    src: "https://i.ibb.co/sQDrpQh/figma-1.png",
+    src: figma,
     className: "skillsIcon",
   },
   {
     id: 6,
     title: "Email on Acid",
-    src: "https://i.ibb.co/hV7btYB/My-project-1.png",
+    src: emailOnAcid,
     className: "skillsIcon",
   },
   {
     id: 7,
-    title: "Litmus",
-    src: "https://i.ibb.co/wrRJJv6/litmus.png",
+    title: "Wordpress",
+    src: wordpress,
     className: "skillsIcon",
   },
   {
     id: 8,
-    title: "Wordpress",
-    src: "https://i.ibb.co/q1MMwVr/wordpress-icon.png",
+    title: "Git",
+    src: git,
     className: "skillsIcon",
   },
   {
     id: 9,
-    title: "Git",
-    src: "https://i.ibb.co/NWDvj3m/black-github.png",
+    title: "Tailwind CSS",
+    src: tailwind,
     className: "skillsIcon",
   },
-  // {
-  //   id: 10,
-  //   title: "TypeScript",
-  //   src: "https://i.ibb.co/1TVg8HR/typescript-icon.png",
-  //   className: "skillsIcon",
-  // },
+  {
+    id: 10,
+    title: "Linux",
+    src: linux,
+    className: "skillsIcon",
+  },
+  {
+    id: 11,
+    title: "React",
+    src: react,
+    className: "skillsIcon",
+  },
   {
     id: 12,
-    title: "Tailwind CSS",
-    src: "https://i.ibb.co/1rK09Qy/tailwind.png",
-    className: "skillsIcon",
-  },
-  {
-    id: 13,
-    title: "Linux",
-    src: "https://i.ibb.co/HphyG1z/linux.png",
-    className: "skillsIcon",
-  },
-  {
-    id: 14,
-    title: "React",
-    src: "https://i.ibb.co/0jwjv3Y/react-icon.png",
-    className: "skillsIcon",
-  },
-  // {
-  //   id: 15,
-  //   title: "Bash",
-  //   src: "https://i.ibb.co/WVZ1Qhz/bash.png",
-  //   className: "skillsIcon",
-  // },
-  {
-    id: 16,
     title: "Wireshark",
-    src: "https://i.ibb.co/bbBPdPy/wireshark-icon.png",
+    src: wireshark,
     className: "skillsIcon",
   },
 ];
 
 export default data;
-
-/*
-
-next js
-
-
-
-https://i.ibb.co/tqMB96L/mailchimp-freddie-icon.png
-
-https://i.ibb.co/sQs2kkF/salesforce-2.png
-
-
-
-
-https://i.ibb.co/zmp9kV9/black-linkedin.png
-
-
-
-updated 20/5/2024
-direct links
-https://i.ibb.co/xj1R7gV/ubuntu.png
-https://i.ibb.co/hLvBWkm/jenkins-logo.png
-https://i.ibb.co/jwf5G4M/splunk-icon.png
-https://i.ibb.co/bbBPdPy/wireshark-icon.png
-https://i.ibb.co/V3rKNy7/webpack-icon.png
-https://i.ibb.co/XZy8FsY/debian.png
-https://i.ibb.co/wyLFq33/docker.png
-https://i.ibb.co/Qb6dgdb/nodejs.png
-https://i.ibb.co/9VFqvyc/mondodb.png
-https://i.ibb.co/WVZ1Qhz/bash.png
-https://i.ibb.co/HphyG1z/linux.png
-https://i.ibb.co/N75qD4S/python-icon.png
-https://i.ibb.co/1TVg8HR/typescript-icon.png
-https://i.ibb.co/Jp4qfFy/nextjs-icon.png
-https://i.ibb.co/0jwjv3Y/react-icon.png
-https://i.ibb.co/1rK09Qy/tailwind.png
-
-
-
-// Full links
-<a href="https://ibb.co/tqcHbKj"><img src="https://i.ibb.co/1rK09Qy/tailwind.png" alt="tailwind" border="0"></a>
-<a href="https://ibb.co/Xs7vC5m"><img src="https://i.ibb.co/xj1R7gV/ubuntu.png" alt="ubuntu" border="0"></a>
-<a href="https://ibb.co/HBQtz0H"><img src="https://i.ibb.co/hLvBWkm/jenkins-logo.png" alt="jenkins-logo" border="0"></a>
-<a href="https://ibb.co/DMWbkKC"><img src="https://i.ibb.co/jwf5G4M/splunk-icon.png" alt="splunk-icon" border="0"></a>
-<a href="https://ibb.co/6XPmsmp"><img src="https://i.ibb.co/bbBPdPy/wireshark-icon.png" alt="wireshark-icon" border="0"></a>
-<a href="https://ibb.co/DLsqbXx"><img src="https://i.ibb.co/V3rKNy7/webpack-icon.png" alt="webpack-icon" border="0"></a>
-<a href="https://ibb.co/TBKLP8W"><img src="https://i.ibb.co/XZy8FsY/debian.png" alt="debian" border="0"></a>
-<a href="https://ibb.co/5T9g3tt"><img src="https://i.ibb.co/wyLFq33/docker.png" alt="docker" border="0"></a>
-<a href="https://ibb.co/yf8S9Sf"><img src="https://i.ibb.co/Qb6dgdb/nodejs.png" alt="nodejs" border="0"></a>
-<a href="https://ibb.co/KLM9GVb"><img src="https://i.ibb.co/9VFqvyc/mondodb.png" alt="mondodb" border="0"></a>
-<a href="https://ibb.co/qJKwzSg"><img src="https://i.ibb.co/WVZ1Qhz/bash.png" alt="bash" border="0"></a>
-<a href="https://ibb.co/CsPyVN7"><img src="https://i.ibb.co/HphyG1z/linux.png" alt="linux" border="0"></a>
-<a href="https://ibb.co/Fn21ZPV"><img src="https://i.ibb.co/N75qD4S/python-icon.png" alt="python-icon" border="0"></a>
-<a href="https://ibb.co/CQdchxV"><img src="https://i.ibb.co/1TVg8HR/typescript-icon.png" alt="typescript-icon" border="0"></a>
-<a href="https://ibb.co/c8HwGXC"><img src="https://i.ibb.co/Jp4qfFy/nextjs-icon.png" alt="nextjs-icon" border="0"></a>
-<a href="https://ibb.co/Rp5pdWQ"><img src="https://i.ibb.co/0jwjv3Y/react-icon.png" alt="react-icon" border="0"></a>
-*/
