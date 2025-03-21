349ccbc884331e18936e5ccd072fc82a91d9c8dc
Eric Chun <eric.chun88800888@gmail.com>
2025-03-21 12:15:12 +0900

    Updated profile

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -37,20 +37,19 @@ const About = ({ theme }) => {
           </div>
           <div className='bio-container'>
             <p className='bio-info'>
-              Hi, I&apos;m Eric, a language educator, frontend developer and
-              content creator with a passion for empowering others. With a
-              strong background in education and teaching, I focus on building
-              intuitive digital experiences that are both engaging and
-              accessible. As an Australian currently based in Osaka, I bring a
-              unique global persective to my work. I'm also deeply interested in
-              the Ed-Tech, E-Commerce and GRC cybersecurity sectors as it blends
-              my love for technology, business and education.
+              Hi, I&apos;m Eric, a frontend developer, content creator and
+              tinkerer. With a strong background in education and business
+              industry, I focus on building intuitive digital user experiences
+              that are both engaging and accessible. As an Australian currently
+              based in Osaka, I bring a unique global persective to my work. I'm
+              also deeply interested in blockchain payments and the
+              cybersecurity sector.
             </p>
             <p className='bio-info'>
               Whether I'm coding new features or sharing insights through
               educational content, my goal is to always create value for my
-              clients and students. Feel free to get in touch with me or
-              consider hiring me for your next project.
+              clients. Feel free to get in touch with me or consider hiring me
+              for your next project.
             </p>
             <div className='button-container'>
               <a href='#contact'>
