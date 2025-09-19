a780cc003d1ad0820333811e1c45314e065c0822
Eric Chun <eric.chun88800888@gmail.com>
2025-09-19 10:29:56 +0900

    updated footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = ({ theme }) => {
                 />
               </div>
               <p className='short_description'>
-                Content Strategist & Producer • Web & E-Learning Developer • Instructional Design 
+                Content Strategist & Producer • Web & E-Learning Developer • Instructional Design • Learning & Design  
               </p>
             </div>
           </div>
